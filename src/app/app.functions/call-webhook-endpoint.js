b469db7a73a8201f0bc72648919ee033e9b35a82
zhangl32 <lu.zhang@lixil.com>
2024-08-28 14:13:37 +0900

    Update call-webhook-endpoint.js

--- src/app/app.functions/call-webhook-endpoint.js
+++ src/app/app.functions/call-webhook-endpoint.js
@@ -5,7 +5,7 @@ exports.main = async (context = {}) => {
   const { saleslotId } = context.parameters;
   
   try {
-    await callWebhookEndpoint(saleslotId);
+    await callWebhookEndpoint(saleslotId1);
     return { status: 'success', meesage:"Workflow initiated successfully. Please wait for 4-5 minutes while the system completes the processing.  Thank you for your patience!" };
   } catch (err) {
     return { status: 'error', message: err.message }
@@ -14,7 +14,7 @@ exports.main = async (context = {}) => {
 
 const callWebhookEndpoint = async (saleslotId) => {
   return axios.post(
-    `https://api-na1.hubapi.com/automation/v4/webhook-triggers/39703267/Fgue9nS1`,
+    `https://api-na1.hubapi.com/automation/v4/webhook-triggers/39703267/Fgue9nS`,
     {
         saleslotId: saleslotId,
     },
