9efef2a7a344a0d1acc458d901e03c6ef16e2fae
zhangl32 <lu.zhang@lixil.com>
2024-08-29 16:16:33 +0900

    Update call-webhook-endpoint.js

--- src/app/app.functions/call-webhook-endpoint.js
+++ src/app/app.functions/call-webhook-endpoint.js
@@ -1,16 +1,51 @@
 // For HubSpot API calls
 const axios = require('axios');
+const hubspot = require('@hubspot/api-client');
 
 exports.main = async (context = {}) => {
-  const { saleslotId } = context.parameters;
+  const hubspotClient = new hubspot.Client({
+    accessToken: process.env['PRIVATE_APP_ACCESS_TOKEN'],
+  });
   
+  // Deal ObjectTypeId
+  const objectTypeDeal = "0-3";
+  // SalesLot ObjectTypeId
+  const objectTypeSalesLot = "2-32224851";
+  // process Sales RecordId
+  const {saleslotId} = context.parameters;
+  // process Deal RecordId
+  let dealRecordId = "";
+
   try {
-    await callWebhookEndpoint(saleslotId);
-    return { status: 'success', meesage:"Workflow initiated successfully. Please wait for 4-5 minutes while the system completes the processing.  Thank you for your patience!" };
+    //-------------------------SalesLotのRecordIDで関連すべてDealのRecordIDを取得する -------------------------
+    const BatchInputPublicObjectId = { inputs: [{"id":saleslotId}] };
+    let apiResponse = await hubspotClient.crm.associations.batchApi.read(objectTypeSalesLot, objectTypeDeal, BatchInputPublicObjectId);
+    if(apiResponse.results.length > 0 ){
+      let associations = apiResponse.results[0]["to"];
+      if(associations.length > 0){
+        dealRecordId = associations[0].id;
+      }
+    }
+    
+    let dealstage = '';
+    if(dealRecordId != ''){
+      const dealProperties = ["dealstage"];
+      let apiDealResponse = await hubspotClient.crm.deals.basicApi.getById(dealRecordId, dealProperties);
+      dealstage =apiDealResponse.properties.dealstage;
+    }
+
+    //-------------------------対象データを一括作成-------------------------
+    if(dealstage=='75418032'){
+      await callWebhookEndpoint(saleslotId);
+      return { status: 'success', meesage:"Workflow initiated successfully. Please wait for 4-5 minutes while the system completes the processing.  Thank you for your patience!" };
+    }
+    return { status: 'success', message: "Can't copy current saleslot because it's deal stage is not [W : Contracted]." }
+    
   } catch (err) {
     return { status: 'error', message: err.message }
   }
 };
+
 
 const callWebhookEndpoint = async (saleslotId) => {
   return axios.post(
